e5c1c3d1f3151134af76581f0bdd9a126309e866
gaizhongfeng <gaizhongfeng@yeah.net>
2023-09-22 09:33:04 +0800

    hello world

---
+++ src/com/fast/helloWorld.java
@@ -0,0 +1,10 @@
+package com.fast;
+
+public class helloWorld {
+    public static void sayHello(){
+        System.out.println("hello!!!");
+    }
+    public static void main(String[] args){
+        sayHello();
+    }
+}
